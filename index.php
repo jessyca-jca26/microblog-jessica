47facba364ded3e0d9bd5dc2df5812734eacd1a4
jessyca-jca26 <jessyca.jca26@gmail.com>
2022-06-09 16:19:27 -0300

    Banco de Dados- Posts

--- index.php
+++ index.php
@@ -1,38 +1,27 @@
 <?php
 require "inc/cabecalho.php";
+require "inc/funcoes-posts.php";
+$post = lerTodosOsPosts($conexao);
 ?>
 
 <div class="row my-1 mx-md-n1">
   <!-- INÍCIO ROW -->
-
+  <?php foreach($post as $post) { ?>
   <!-- INÍCIO Card -->
   <div class="col-md-6 my-1 px-md-1">
     <article class="card shadow-sm h-100">
-      <a href="post-detalhe.php" class="card-link">
-        <img class="card-img-top" src="imagens/provisoria1.jpg" alt="Imagem de destaque do post">
+      <a href="post-detalhe.php?id=<?=$post['id']?> " class="card-link">
+        <img class="card-img-top" src="imagens/<?=$post['imagem']?>" alt="Imagem de destaque do post">
         <div class="card-body">
-          <h5 class="card-title">Título do post...</h5>
-          <p class="card-text">Resumo do post...</p>
+          <h5 class="card-title"><?=$post['titulo']?></h5>
+          <p class="card-text"><?=$post['resumo']?></p>
         </div>
       </a>
     </article>
   </div>
+  <?php } ?>
   <!-- FIM Card -->
 
-  <!-- INÍCIO Card -->
-  <div class="col-md-6 my-1 px-md-1">
-    <article class="card shadow-sm h-100">
-      <a href="post-detalhe.php" class="card-link">
-        <img class="card-img-top" src="imagens/provisoria2.jpg" alt="Imagem de capa do card">
-        <div class="card-body">
-          <h5 class="card-title">Título do post...</h5>
-          <p class="card-text">Resumo do post...</p>
-        </div>
-      </a>
-    </article>
-  </div>
-  <!-- FIM Card -->
-
-</div> <!-- FIM ROW -->
+  
 
 <?php require "inc/rodape.php"; ?>
